007b2f9eaeafd9f10db06dd309a83a159152fc7b
Yujeong Shin <83907194+shinin9@users.noreply.github.com>
2023-10-27 18:28:12 +0900

    Delete 003 Code/DL directory

--- 003 Code/DL/yolo/yolo_predict.py
+++
@@ -1,8 +0,0 @@
-from ultralytics import YOLO
-
-model = YOLO("/home/com_2/workspace/yjshin/capstone/results/runs/detect/train2/weights/best.pt")
-
-results = model.predict(source="/home/com_2/workspace/pig_video/pig_lsw.mp4", save=True)
-
-for r in results:
-    print(r.boxes)
